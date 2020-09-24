c2ca4aa93b9ae9a46f18d1d0af2619789e907ba5
svvishjs <andreww92@live.com>
2020-09-24 10:12:31 +0100

    added a function to controller.py

--- my_web_app/controllers/controller.py
+++ my_web_app/controllers/controller.py
@@ -1,9 +1,13 @@
 from app import app
 
 @app.route('/')
-def index():
+def blablabla():
     return "Something DEAD cool."
 
 @app.route('/<name>')
 def greet(name):
     return f"Hello {name}!"
+
+@app.route('/h/a')
+def yp():
+    return "Hello james!"
